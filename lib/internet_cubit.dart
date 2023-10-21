c8233ff2e932bcc3b2f6085713b293996b9e37e2
Ali jawad <97254328+AliJawadSubhan@users.noreply.github.com>
2023-10-21 14:43:42 +0500

    add wifi service

---
+++ lib/internet_cubit.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class InternetCubit extends Cubit<InternetState> {
+  StreamSubscription? streamSubscription;
+  final Connectivity _connectivity = Connectivity();
+  InternetCubit() : super(InternetState.init) {
+    streamSubscription = _connectivity.onConnectivityChanged.listen((event) {
+      if (event == ConnectivityResult.wifi ||
+          event == ConnectivityResult.mobile) {
+        emit(InternetState.gain);
+      } else {
+        emit(InternetState.error);
+      }
+    });
+  }
+}
+
+enum InternetState { init, gain, error }
